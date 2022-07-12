132809e39ece6c0f2b1d86716c3a347b6ab825b3
Juliano Maia <leite.juliano@gmail.com>
2022-07-11 21:48:23 -0300

    feat: remove text editing

--- apps/landing-page/pages/index.js
+++ apps/landing-page/pages/index.js
@@ -13,10 +13,6 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
