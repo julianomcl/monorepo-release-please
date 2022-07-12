de7b50288d7569334edbaac80f81082f9e388b14
Juliano Maia <leite.juliano@gmail.com>
2022-07-11 22:09:10 -0300

    feat!: remove texts

--- apps/landing-page/pages/index.js
+++ apps/landing-page/pages/index.js
@@ -23,24 +23,6 @@ export default function Home() {
             <h3>Aprenda &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
